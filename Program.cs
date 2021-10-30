d55ff1e144837cac6f3083b9bc227f56ad3de2ff
Marius <marius_taboo@yahoo.com>
2021-10-30 19:35:29 +0100

    add new method

--- Program.cs
+++ Program.cs
@@ -7,11 +7,40 @@ namespace UsePackages
     {
         static void Main(string[] args)
         {
-            var number = 234;
+            DisplayResults();
+        }
 
-            var result = FizzBuzz.GetResult(number);
+        public static void DisplayResults()
+        {
+            const int LINE = 3;
+            int[] arr = new int[] { 1, 4, 11, 33, 6, 9, 12, 21, 33, 25, -5, 5, 10, 55, 100, 15 };
 
-            Console.WriteLine("FizzBuzz Result is: " + result);
+            var index = 0;
+            var occurrence = 0;
+
+            do
+            {
+                if (occurrence == LINE) 
+                {
+                    occurrence = 0;
+                    Console.WriteLine();
+                }
+
+                var result = FizzBuzz.GetResult(arr[index]);
+
+                if (result == "FizzBuzz") 
+                {
+                    Console.WriteLine(" !!! FizzBuzz !!! ");
+
+                    return;
+                }
+
+                occurrence++;
+                Console.Write($"{result}   ");
+
+                index++;
+
+            } while (arr.Length > index);
         }
     }
 }
